a7e8922f4293572b6e09c7c80bef31f87618e4aa
yongjunhan <hanyongjun.dev@gmail.com>
2023-03-07 17:16:38 +0900

    stateful , setState

---
+++ stateful/statefull/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'click',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+              ),
+              Text(
+                '$counter',
+                style:
+                    const TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+              ),
+              IconButton(onPressed: onClicked, icon: const Icon(Icons.add))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
